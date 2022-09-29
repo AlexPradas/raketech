e14ef5db7ab5bf0cc02b28447b0cf5bce429c631
alexpradas <alferpra@gmail.com>
2022-09-29 13:52:36 +0200

    Create Raketech plugin

---
+++ wp-content/plugins/raketech/raketech.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Raketech
+ * Description: WordPress Developer Coding Exercise
+ * Version: 1.0
+ * Author: Alejandro Fernández Pradas
+ * Author URI: https://alexpradas.com
+ * License: GPL2
+ */
