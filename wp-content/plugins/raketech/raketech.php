6f9bb1c67d15cc8166decd11fed04af1efcf2679
alexpradas <alferpra@gmail.com>
2022-09-29 13:58:02 +0200

    Enqueue plugin styles and font-awesome

--- wp-content/plugins/raketech/raketech.php
+++ wp-content/plugins/raketech/raketech.php
@@ -7,3 +7,25 @@
  * Author URI: https://alexpradas.com
  * License: GPL2
  */
+
+
+if( ! defined( 'ABSPATH') ){
+    exit;
+}
+
+
+if ( ! function_exists( 'raketech_enqueue_style' ) ) :
+
+   /**
+    * Enqueue default styles for Raketech Plugin and Font Awesome icons.
+    *
+    * @return void
+    */
+
+   function raketech_enqueue_style() {
+       wp_enqueue_style( 'raketech-style', plugins_url('/raketech/assets/css/style.css'), array(), rand()); 
+       wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css' );
+   }
+endif;
+
+add_action( 'wp_enqueue_scripts', 'raketech_enqueue_style' );
