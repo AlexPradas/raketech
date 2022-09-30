368e21c32bdf1346a843485eade78002879c93b1
alexpradas <alferpra@gmail.com>
2022-09-30 13:42:29 +0200

    Reorder functions and correct styles

--- wp-content/plugins/raketech/raketech.php
+++ wp-content/plugins/raketech/raketech.php
@@ -32,6 +32,33 @@ add_action( 'wp_enqueue_scripts', 'raketech_enqueue_style' );
 
 
 
+if ( ! function_exists( 'raketech_get_final_url' ) ) :
+
+    /**
+     * Get last url after redirection
+     *
+     * @param string $url Nav menu item start element.
+     *
+     *
+     * @return string Final url after redirects.
+     */
+    function raketech_get_final_url($url) {
+        $curlhandle = curl_init();
+        curl_setopt($curlhandle, CURLOPT_URL, $url);
+        curl_setopt($curlhandle, CURLOPT_HEADER, 1);
+        curl_setopt($curlhandle, CURLOPT_USERAGENT, 'googlebot');
+        curl_setopt($curlhandle, CURLOPT_FOLLOWLOCATION, 0);
+        curl_setopt($curlhandle, CURLOPT_RETURNTRANSFER, 1);
+        $final = curl_exec($curlhandle);
+        if (preg_match('~Location: (.*)~i', $final, $lasturl)) {
+            $loc = trim($lasturl[1]);
+            return $loc;
+        } 
+        else {
+            return $loc= $url; 
+        }
+    }
+endif;
 
 
 if ( ! function_exists( 'raketech_get_reviews' ) ) :
@@ -51,35 +78,6 @@ if ( ! function_exists( 'raketech_get_reviews' ) ) :
      *           'terms_and_conditions')
 	 */
 
-    if ( ! function_exists( 'raketech_get_final_url' ) ) :
-
-        /**
-         * Get last url after redirection
-         *
-         * @param string $url Nav menu item start element.
-         *
-         *
-         * @return string Final url after redirects.
-         */
-        function raketech_get_final_url($url) {
-            $curlhandle = curl_init();
-            curl_setopt($curlhandle, CURLOPT_URL, $url);
-            curl_setopt($curlhandle, CURLOPT_HEADER, 1);
-            curl_setopt($curlhandle, CURLOPT_USERAGENT, 'googlebot');
-            curl_setopt($curlhandle, CURLOPT_FOLLOWLOCATION, 0);
-            curl_setopt($curlhandle, CURLOPT_RETURNTRANSFER, 1);
-            $final = curl_exec($curlhandle);
-            if (preg_match('~Location: (.*)~i', $final, $lasturl)) {
-                $loc = trim($lasturl[1]);
-                return $loc;
-            } 
-            else {
-                return $loc= $url; 
-            }
-        }
-    endif;
-
-
     function raketech_get_reviews() {
         $request_reviews = wp_remote_get('https://alexpradas.com/wp-content/themes/Divi/raketechapi.json' );
         $response_reviews = wp_remote_retrieve_body($request_reviews);
@@ -87,13 +85,13 @@ if ( ! function_exists( 'raketech_get_reviews' ) ) :
         $raketech_reviews = $data_reviews->{'toplists'}->{'575'};
         foreach ($raketech_reviews as $widget){
             $logo= raketech_get_final_url($widget->{'logo'});
-            $brand_id =   $widget->{'brand_id'};
-            $rating = $widget->{'info'}->{'rating'};
-            $bonus = $widget->{'info'}->{'bonus'};
-            $play_url = $widget->{'play_url'};
-            $terms_and_conditions = $widget->{'terms_and_conditions'};
-            $features = $widget->{'info'}->{'features'};
-            $position = $widget->{'position'};
+            $brand_id =   $widget->brand_id;
+            $rating = $widget->info->rating;
+            $bonus = $widget->info->bonus;
+            $play_url = $widget->play_url;
+            $terms_and_conditions = $widget->terms_and_conditions;
+            $features = $widget->info->features;
+            $position = $widget->position;
 
             $raketech_reviews_array[] = array(
                 'position' => $position, 
